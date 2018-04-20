55b8ee3acb673491f912a62e0b060a164999c4de
Yuzhou "npfjcg Yukino" Chen <npfjcg@163.com>
2018-04-20 09:20:44 +0000

    Update start.py

--- start.py
+++ start.py
@@ -11,9 +11,26 @@ from urllib import request
 import os
 import re
 import random
-inputs = input('请输入你要查询的tag(可以有多个)')
+inputs = input('请输入你要查询的tag(可以有多个):')
+#选择图片来源
+src_i = input('请输入图片来源，1.Gelbooru,2.Yande.re(默认为Gelbooru):')
+if src_i == '1':
+    source = r'http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags='
+elif src_i == '2':
+    source = r'https://yande.re/post.xml?tags='
+else:
+    source = r'http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=-comic -monochrome'
+    src_i='1'
+#选择图片分级
+rate_i = input('请输入图片分级，1.safe，2.questionable和safe，3.explicit+safe+questionable，(默认为safe):')
+if rate_i == '1':
+    rate=' rating:safe'
+elif rate_i == '2':
+    rate=' -rating:explicit'
+else:
+    rate=' '
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'} 
-r = requests.get(r'http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=rating%3asafe '+inputs,headers=headers) 
+r = requests.get(source+inputs+rate+' -comic',headers=headers) 
 #以伪装的Header爬取Gelbooru相关tag内容
 if r.status_code == 200:
     soup = BeautifulSoup(r.text, "lxml")
@@ -23,22 +40,34 @@ if r.status_code == 200:
     maxpage = int(round(num/100))
     #查看页面上有多少图片，以100张图一页的话有多少图
     count=0
-    print('Gelbooru API访问正常...\n对于tag:'+inputs+' rating:safe,有'+str(num)+'张图，'+str(maxpage)+'页')
+    print('API访问正常...\n对于tag:'+inputs+rate+',有'+str(num)+'张图，'+str(maxpage)+'页')
     if num == 0:
         print('没有找到图片，可能是出错了...')
         #找到所有的图片
         #如果没有图片就说明出问题了
     else:
-        for page in range(0,maxpage):
+        for page in range(0,maxpage+1):
         #爬取所有页面的图，所以在每一页都要找到图片
-            pgs=requests.get(r'http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags='+inputs+' rating:safe&pid='+str(page))
+            if src_i == '1':
+                pgs=requests.get(source+inputs+rate+'&pid='+str(page))
+            elif src_i == '2':
+                pgs=requests.get(source+inputs+rate+'&page'+str(page))
             if pgs.status_code == 200:
                 getcha=BeautifulSoup(pgs.text,"lxml")
                 posts=getcha.find('posts')
                 pics=posts.find_all('post')
                 for link in pics:
-                    request.urlretrieve(link['file_url'], r'%s' % link['file_url'][40:])
-                    count=count+1
+                #不同网站使用不同爬取规则
+                    if src_i == '1':
+                        request.urlretrieve(link['file_url'], r'%s' % link['file_url'][40:])
+                        count=count+1
+                        print('已成功下载'+link['file_url'][40:]+',共'+str(num)+'张图,还剩'+str(num-count)+'张,现在位于第'+str(page)+'页')
+                    elif src_i == '2':
+                        request.urlretrieve(link['file_url'], r'%s' % link['file_url'][62:])
+                        count=count+1
+                        print('已成功下载'+link['file_url'][62:]+',共'+str(num)+'张图,还剩'+str(num-count)+'张,现在位于第'+str(page)+'页')
                     #图片计数加一
-                    print('已成功下载'+link['file_url'][40:]+',共'+str(num)+'张图,还剩'+str(num-count)+'张,现在位于第'+str(page)+'页')
-                print('第'+str(page)+'页已经爬取完毕，下一页是'+str(page+1))
+                if page+1 == maxpage:
+                    print('第'+str(page)+'页已经爬取完毕，爬取完成，共获取了'+str(count)+'张图')
+                else:
+                    print('第'+str(page)+'页已经爬取完毕，下一页是第'+str(page+1)+'页')
